d975120591f773ffebb768f760b0aaaec9a2e076
Jordy Rose <jediknil@belkadan.com>
2011-01-23 23:19:53 +0000

    Update copyrights. (Oops, last time said 2010.)
    git-svn-id: svn+belkadan://belkadan.com/jr/cocoa/webmailer/release@171 ba59ac2a-6797-437e-b805-98a2da23c0da

--- FullWidthView.m
+++ FullWidthView.m
@@ -1,5 +1,5 @@
 /*******************************************************************************
- Copyright (c) 2009 Jordy Rose
+ Copyright (c) 2009-2011 Jordy Rose
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
--- ScriptingBridgeApps.h
+++ ScriptingBridgeApps.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
- Copyright (c) 2010 Jordy Rose
+ Copyright (c) 2010-2011 Jordy Rose
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
